d499c2d5e4d274fd3e8d3a9ea4aa4b220542b881
loveadmire <loveadmire@126.com>
2019-03-26 10:05:54 +0800

    数据处理部分

--- src/algorithm/judge_num.c
+++ src/algorithm/judge_num.c
@@ -2,6 +2,8 @@
 // Created by root on 19-3-23.
 //
 #include "../../inc/algorithm/judge_num.h"
+#include <stdio.h>
+#include <math.h>
 
 int Judge_num_isOdd(int num){
     if (num > 0)
@@ -18,7 +20,7 @@ int Judge_num_isPrime(int num){
     if (num %6 != 1 && num %6 != 5){
         return 0;
     }
-    float ss = (float)sqrtf128(num);
+    float ss = (float)sqrt(num);
     printf(" ss is %02f ",ss);
     int temp = (int)sqrtf(num);
     int i = 0;
